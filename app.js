9e07b8165b3e6512685036366286faff269a6942
DanielCorcoran <corky1@gmail.com>
2019-06-24 19:27:34 -0700

    Add route to post status and status update form

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const posts = [
+    {author: "Steve", status: "Doing things"},
+    {author: "Jake", status: "Enjoying the great weather"},
+    {author: "Jess", status: "Dig up! - Albert Einstein"}
+];
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/feed", (req, res) => {
-    const posts = [
-        {author: "Steve", status: "Doing things"},
-        {author: "Jake", status: "Enjoying the great weather"},
-        {author: "Jess", status: "Dig up! - Albert Einstein"}
-    ]
-
     res.render("feed", {posts: posts});
 });
 
+app.post("/feed", (req, res) => {
+    const status = req.body.status;
+    const newStatus = {status: status};
+    posts.push(newStatus);
+    res.redirect("/feed");
+});
+
+app.get("/feed/new", (req, res) => {
+    res.render("new.ejs");
+});
 
 app.listen(process.env.PORT || 8080, () => { 
     console.log("Server has started");
