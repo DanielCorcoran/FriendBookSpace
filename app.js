f8ee98f39f2f2063a5d6c5b9a57b5c43b046a008
Dan <danielccorcoran@gmail.com>
2019-11-26 15:27:55 -0800

    Update landing page

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 const express = require("express"),
-      app = express(),
-      bodyParser = require("body-parser"),
-      mongoose = require("mongoose"),
-      flash = require("connect-flash"),
-      passport = require("passport"),
-      LocalStrategy = require("passport-local"),
-      methodOverride = require("method-override"),
-      User = require("./models/user"),
-      seedDB = require("./seeds");
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
 const feedRoutes = require("./routes/feed"),
-      indexRoutes = require("./routes/index");
+  indexRoutes = require("./routes/index");
 
 // seedDB();
-mongoose.connect("mongodb://localhost/FriendBookSpace", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/FriendBookSpace", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 mongoose.set("useFindAndModify", false);
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "FriendBookSpace will revolutionize social media!",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -33,15 +38,15 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/feed", feedRoutes);
 app.use(indexRoutes);
 
-app.listen(process.env.PORT || 8080, () => { 
-    console.log("Server has started");
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server has started");
 });
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,50 @@
 const express = require("express"),
-      router = express.Router(),
-      passport = require("passport"),
-      User = require("../models/user");
+  router = express.Router(),
+  passport = require("passport"),
+  User = require("../models/user");
 
 router.get("/", (req, res) => {
+  if (!req.isAuthenticated()) {
     res.render("landing");
+  } else {
+    res.redirect("/feed");
+  }
 });
 
 router.get("/register", (req, res) => {
-    res.render("register");
+  res.render("register");
 });
 
 router.post("/register", (req, res) => {
-    const newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, (err, user) => {
-        if (err) {
-            req.flash("error", err.message);            
-            return res.redirect("/register");
-        }
-        passport.authenticate("local")(req, res, () => {
-            req.flash("success", "Welcome to FriendBookSpace!");
-            res.redirect("/feed");
-        });
+  const newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      req.flash("error", err.message);
+      return res.redirect("/register");
+    }
+    passport.authenticate("local")(req, res, () => {
+      req.flash("success", "Welcome to FriendBookSpace!");
+      res.redirect("/feed");
     });
+  });
 });
 
 router.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
-router.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/feed",
-        failureRedirect: "/login"
-    })
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/feed",
+    failureRedirect: "/login"
+  })
 );
 
 router.get("/logout", (req, res) => {
-    req.logout();
-    req.flash("success", "Successfully Logged Out");
-    res.redirect("/");
+  req.logout();
+  req.flash("success", "Successfully Logged Out");
+  res.redirect("/");
 });
 
 module.exports = router;
