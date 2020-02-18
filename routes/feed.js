c7bcb8e52582502a143527e09efa1a7df12990e4
Dan <danielccorcoran@gmail.com>
2020-02-18 15:31:01 -0800

    Format and refactor source

--- routes/feed.js
+++ routes/feed.js
@@ -1,53 +1,38 @@
 const express = require("express"),
 	router = express.Router(),
-	User = require("../models/user"),
 	Status = require("../models/status"),
 	Comment = require("../models/comment"),
 	middleware = require("../middleware");
 
 router.get("/", middleware.isLoggedIn, (req, res) => {
 	if (!req.user.following.length) {
-		User.find({})
-			.limit(20)
-			.exec((err, userList) => {
-				if (err) {
-					req.flash("error", "Could not load user list");
-					res.redirect("/");
-				} else {
-					res.render("findUsers", {
-						currentUser: req.user,
-						userList: userList,
-						isFollowingSomeone: false
-					});
-				}
-			});
-	} else {
-		Status.find({})
-			.sort({ createdAt: -1 })
-			.populate("comments")
-			.exec((err, statuses) => {
-				if (err) {
-					req.flash("error", "Could not load status feed");
-					res.redirect("/");
-				} else {
-					const statusesToPass = [];
-
-					statuses.forEach(status => {
-						if (
-							req.user.following.includes(status.author.id) ||
-							status.author.id.equals(req.user._id)
-						) {
-							statusesToPass.push(status);
-						}
-					});
-
-					res.render("index", {
-						statuses: statusesToPass,
-						currentUser: req.user
-					});
-				}
-			});
+		res.redirect("findUsers");
 	}
+
+	Status.find({})
+		.sort({ createdAt: -1 })
+		.populate("comments")
+		.exec((err, statuses) => {
+			if (err) {
+				flashMsg(req, res, false, "Could not load status feed", "/");
+			}
+
+			const statusesToPass = [];
+
+			statuses.forEach(status => {
+				if (
+					req.user.following.includes(status.author.id) ||
+					status.author.id.equals(req.user._id)
+				) {
+					statusesToPass.push(status);
+				}
+			});
+
+			res.render("index", {
+				statuses: statusesToPass,
+				currentUser: req.user
+			});
+		});
 });
 
 router.post("/", middleware.isLoggedIn, (req, res) => {
@@ -60,8 +45,9 @@ router.post("/", middleware.isLoggedIn, (req, res) => {
 
 	Status.create(newStatus, err => {
 		if (err) {
-			req.flash("error", "Error creating status");
+			flashMsg(req, res, false, "Error creating status", "/feed");
 		}
+
 		res.redirect("/feed");
 	});
 });
@@ -71,35 +57,37 @@ router.get("/:id", middleware.isLoggedIn, (req, res) => {
 		.populate("comments")
 		.exec((err, foundStatus) => {
 			if (err) {
-				req.flash("error", "Status not found");
-				res.redirect("back");
-			} else {
-				res.render("comment", { status: foundStatus, currentUser: req.user });
+				flashMsg(req, res, false, "Status not found", "back");
 			}
+
+			res.render("comment", { status: foundStatus, currentUser: req.user });
 		});
 });
 
 router.post("/:id", middleware.isLoggedIn, (req, res) => {
 	Status.findById(req.params.id, (err, status) => {
 		if (err) {
-			req.flash("error", "Status not found");
-			res.redirect("/feed");
-		} else {
-			Comment.create(req.body.comment, (err, comment) => {
-				if (err) {
-					req.flash("error", "Error creating comment");
-					res.redirect("/feed");
-				} else {
-					comment.author.id = req.user._id;
-					comment.author.username = req.user.username;
-					comment.save();
-					status.comments.push(comment);
-					status.save();
-					req.flash("success", "Successfully added comment");
-					res.redirect("/feed/" + status._id);
-				}
-			});
+			flashMsg(req, res, false, "Status not found", "/feed");
 		}
+
+		Comment.create(req.body.comment, (err, comment) => {
+			if (err) {
+				flashMsg(req, res, false, "Error creating comment", "/feed");
+			}
+
+			comment.author.id = req.user._id;
+			comment.author.username = req.user.username;
+			comment.save();
+			status.comments.push(comment);
+			status.save();
+			flashMsg(
+				req,
+				res,
+				true,
+				"Successfully added comment",
+				"/feed" + status._id
+			);
+		});
 	});
 });
 
@@ -108,37 +96,33 @@ router.get("/:id/edit", middleware.checkStatusOwnership, (req, res) => {
 		.populate("comments")
 		.exec((err, foundStatus) => {
 			if (err) {
-				req.flash("error", "Status not found");
-				res.redirect("back");
-			} else {
-				res.render("editStatus", {
-					status: foundStatus,
-					currentUser: req.user
-				});
+				flashMsg(req, res, false, "Status not found", "back");
 			}
+
+			res.render("editStatus", {
+				status: foundStatus,
+				currentUser: req.user
+			});
 		});
 });
 
 router.put("/:id", middleware.checkStatusOwnership, (req, res) => {
 	Status.findByIdAndUpdate(req.params.id, { text: req.body.text }, err => {
 		if (err) {
-			req.flash("error", "Status could not be updated");
-		} else {
-			req.flash("success", "Status updated");
+			flashMsg(req, res, false, "Status could not be updated", "/feed");
 		}
-		res.redirect("/feed/");
+
+		flashMsg(req, res, true, "Status updated", "/feed");
 	});
 });
 
 router.delete("/:id", middleware.checkStatusOwnership, (req, res) => {
 	Status.findByIdAndRemove(req.params.id, err => {
 		if (err) {
-			req.flash("error", "Status could not be deleted");
-			res.redirect("back");
-		} else {
-			req.flash("success", "Status deleted");
-			res.redirect("/feed");
+			flashMsg(req, res, false, "Status could not be deleted", "back");
 		}
+
+		flashMsg(req, res, true, "Status deleted", "/feed");
 	});
 });
 
@@ -148,24 +132,22 @@ router.get(
 	(req, res) => {
 		Comment.findById(req.params.commentId, (err, foundComment) => {
 			if (err) {
-				req.flash("error", "Comment not found");
-				res.redirect("back");
-			} else {
-				Status.findById(req.params.id)
-					.populate("comments")
-					.exec((err, foundStatus) => {
-						if (err) {
-							req.flash("error", "Status not found");
-							res.redirect("back");
-						} else {
-							res.render("editComment", {
-								status: foundStatus,
-								commentToEdit: foundComment,
-								currentUser: req.user
-							});
-						}
-					});
+				flashMsg(req, res, false, "Comment not found", "back");
 			}
+
+			Status.findById(req.params.id)
+				.populate("comments")
+				.exec((err, foundStatus) => {
+					if (err) {
+						flashMsg(req, res, false, "Status not found", "back");
+					}
+
+					res.render("editComment", {
+						status: foundStatus,
+						commentToEdit: foundComment,
+						currentUser: req.user
+					});
+				});
 		});
 	}
 );
@@ -176,12 +158,10 @@ router.put("/:id/:commentId", middleware.checkCommentOwnership, (req, res) => {
 		{ text: req.body.text },
 		err => {
 			if (err) {
-				req.flash("error", "Comment could not be updated");
-				res.redirect("back");
-			} else {
-				req.flash("success", "Comment updated");
-				res.redirect("/feed/" + req.params.id);
+				flashMsg(req, res, false, "Comment could not be updated", "back");
 			}
+
+			flashMsg(req, res, true, "Comment updated", "/feed/" + req.params.id);
 		}
 	);
 });
@@ -192,14 +172,25 @@ router.delete(
 	(req, res) => {
 		Comment.findByIdAndRemove(req.params.commentId, err => {
 			if (err) {
-				req.flash("error", "Comment could not be deleted");
-				res.redirect("back");
-			} else {
-				req.flash("success", "Comment deleted");
-				res.redirect("/feed");
+				flashMsg(req, res, false, "Comment could not be deleted", "back");
 			}
+
+			flashMsg(req, res, true, "Comment deleted", "/feed");
 		});
 	}
 );
+
+function flashMsg(req, res, isSuccess, message, route) {
+	let outcome;
+
+	if (isSuccess) {
+		outcome = "success";
+	} else {
+		outcome = "error";
+	}
+
+	req.flash(outcome, message);
+	res.redirect(route);
+}
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,9 @@ const express = require("express"),
 router.get("/", (req, res) => {
 	if (!req.isAuthenticated()) {
 		res.render("landing");
-	} else {
-		res.redirect("/feed");
 	}
+
+	res.redirect("/feed");
 });
 
 router.get("/register", (req, res) => {
@@ -18,13 +18,13 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  const newUser = new User({ username: req.body.username });
-  
+	const newUser = new User({ username: req.body.username });
+
 	User.register(newUser, req.body.password, err => {
 		if (err) {
-			req.flash("error", err.message);
-			return res.redirect("/register");
+			flashMsg(req, res, false, err.message, "/register");
 		}
+
 		passport.authenticate("local")(req, res, () => {
 			flashMsg(req, res, true, "Welcome to FriendBookSpace!", "/feed");
 		});
@@ -48,37 +48,38 @@ router.get("/users/:userId", middleware.isLoggedIn, (req, res) => {
 	User.findById(req.params.userId, (err, foundUser) => {
 		if (err) {
 			flashMsg(req, res, false, "User not found", "/feed");
-		} else {
-			Status.find({ "author.id": foundUser })
-				.sort({ createdAt: -1 })
-				.populate("comments")
-				.exec((err, statuses) => {
-					if (err) {
-						flashMsg(req, res, false, "Something went wrong", "/feed");
-					} else {
-            let isFollowing = false;
-            
-						User.findOne(
-							{ _id: req.user._id, following: req.params.userId },
-							(err, followedUser) => {
-								if (err) {
-									flashMsg(req, res, false, "Something went wrong", "/feed");
-								} else {
-									// You shouldn't be able to follow yourself
-									if (followedUser || foundUser.equals(req.user)) {
-										isFollowing = true;
-									}
-									res.render("user", {
-										statuses: statuses,
-										pageOwner: foundUser,
-										isFollowing: isFollowing
-									});
-								}
-							}
-						);
-					}
-				});
 		}
+
+		Status.find({ "author.id": foundUser })
+			.sort({ createdAt: -1 })
+			.populate("comments")
+			.exec((err, statuses) => {
+				if (err) {
+					flashMsg(req, res, false, "Something went wrong", "/feed");
+				}
+
+				let isFollowing = false;
+
+				User.findOne(
+					{ _id: req.user._id, following: req.params.userId },
+					(err, followedUser) => {
+						if (err) {
+							flashMsg(req, res, false, "Something went wrong", "/feed");
+						}
+
+						// You shouldn't be able to follow yourself
+						if (followedUser || foundUser.equals(req.user)) {
+							isFollowing = true;
+						}
+
+						res.render("user", {
+							statuses: statuses,
+							pageOwner: foundUser,
+							isFollowing: isFollowing
+						});
+					}
+				);
+			});
 	});
 });
 
@@ -86,55 +87,53 @@ router.put("/follow/:userId", middleware.isLoggedIn, (req, res) => {
 	User.findById(req.params.userId, (err, userToFollow) => {
 		if (err) {
 			flashMsg(req, res, false, "User not found", "/feed");
-		} else {
-			User.findOne(
-				{ _id: req.user._id, following: userToFollow },
-				(err, followedUser) => {
-					if (err) {
-						flashMsg(req, res, false, "Something went wrong", "/feed");
-					} else {
-						if (!followedUser && !userToFollow.equals(req.user)) {
-              const successMsg =
-              "You are now following" + userToFollow.username;
-
-							req.user.following.push(userToFollow);
-							req.user.save();
-							flashMsg(req, res, true, successMsg, "/feed");
-						} else {
-							res.redirect("/feed");
-						}
-					}
-				}
-			);
 		}
+
+		User.findOne(
+			{ _id: req.user._id, following: userToFollow },
+			(err, followedUser) => {
+				if (err) {
+					flashMsg(req, res, false, "Something went wrong", "/feed");
+				}
+
+				if (!followedUser && !userToFollow.equals(req.user)) {
+					const successMsg = "You are now following" + userToFollow.username;
+
+					req.user.following.push(userToFollow);
+					req.user.save();
+					flashMsg(req, res, true, successMsg, "/feed");
+				} else {
+					flashMsg(req, res, false, "Could not follow user", "/feed");
+				}
+			}
+		);
 	});
 });
 
 router.delete("/follow/:userId", middleware.isLoggedIn, (req, res) => {
 	User.findById(req.params.userId, (err, userToUnfollow) => {
 		if (err) {
-      flashMsg(req, res, false, "User not found", "/feed");
-		} else {
-			User.findOne(
-				{ _id: req.user._id, following: userToUnfollow },
-				(err, foundUser) => {
-					if (err) {
-						flashMsg(req, res, false, "Something went wrong", "/feed");
-					} else {
-						if (foundUser) {
-              const successMsg =
-              "You unfollowed " + userToUnfollow.username;
-
-							req.user.following.pull(req.params.userId);
-              req.user.save();
-							flashMsg(req, res, true, successMsg, "/feed");
-						} else {
-							flashMsg(req, res, false, "Could not unfollow user", "/feed");
-						}
-					}
-				}
-			);
+			flashMsg(req, res, false, "User not found", "/feed");
 		}
+
+		User.findOne(
+			{ _id: req.user._id, following: userToUnfollow },
+			(err, foundUser) => {
+				if (err) {
+					flashMsg(req, res, false, "Something went wrong", "/feed");
+				}
+
+				if (foundUser) {
+					const successMsg = "You unfollowed " + userToUnfollow.username;
+
+					req.user.following.pull(req.params.userId);
+					req.user.save();
+					flashMsg(req, res, true, successMsg, "/feed");
+				} else {
+					flashMsg(req, res, false, "Could not unfollow user", "/feed");
+				}
+			}
+		);
 	});
 });
 
@@ -144,19 +143,15 @@ router.get("/findusers", middleware.isLoggedIn, (req, res) => {
 			{ username: { $regex: ".*" + req.query.username } },
 			(err, userList) => {
 				if (err) {
-          flashMsg(req, res, false, "Something went wrong", "/feed");
-				} else if (!userList.length) {
-          flashMsg(req, res, false, "No users found", "/feed");
-				} else {
-          const userListToSend = [];
-          
-					userList.forEach(user => {
-						if (!req.user.following.includes(user._id)) {
-							userListToSend.push(user);
-						}
-					});
-					loadFindUsersPage(res, req, userListToSend);
+					flashMsg(req, res, false, "Something went wrong", "/feed");
 				}
+				if (!userList.length) {
+					flashMsg(req, res, false, "No users found", "/findusers");
+				}
+
+				const userListToSend = generateUserListToSend(userList, req);
+
+				loadFindUsersPage(res, req, userListToSend);
 			}
 		);
 	} else {
@@ -164,27 +159,33 @@ router.get("/findusers", middleware.isLoggedIn, (req, res) => {
 			.limit(20)
 			.exec((err, userList) => {
 				if (err) {
-          flashMsg(req, res, false, "Something went wrong", "/feed");
-				} else {
-          const userListToSend = [];
-          
-					userList.forEach(user => {
-						if (!req.user.following.includes(user._id)) {
-							userListToSend.push(user);
-						}
-					});
-					loadFindUsersPage(res, req, userListToSend);
+					flashMsg(req, res, false, "Could not load user list", "/feed");
 				}
+
+				const userListToSend = generateUserListToSend(userList, req);
+
+				loadFindUsersPage(res, req, userListToSend);
 			});
 	}
 });
 
+function generateUserListToSend(userList, req) {
+	const userListToSend = [];
+
+	userList.forEach(user => {
+		if (!req.user.following.includes(user._id)) {
+			userListToSend.push(user);
+		}
+	});
+
+	return userListToSend;
+}
+
 function loadFindUsersPage(res, req, userListToSend) {
-  res.render("findUsers", {
-    currentUser: req.user,
-    userList: userListToSend,
-    isFollowingSomeone: true
-  });
+	res.render("findUsers", {
+		currentUser: req.user,
+		userList: userListToSend
+	});
 }
 
 function flashMsg(req, res, isSuccess, message, route) {
