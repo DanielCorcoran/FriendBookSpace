649697ac74263e3234925c2ad3b123bd6332c3a2
Dan <danielccorcoran@gmail.com>
2020-02-25 14:23:44 -0800

    Refactor; update landing page style

--- middleware/index.js
+++ middleware/index.js
@@ -3,61 +3,74 @@
 // or if he owns a status or comment he is attempting to update.
 
 const Status = require("../models/status"),
-	Comment = require("../models/comment");
+	Comment = require("../models/comment"),
+	util = require("../util.js");
 
 const middlewareObj = {};
+
+
 
 // Checks if a status is owned by the user trying to update that status
 middlewareObj.checkStatusOwnership = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		Status.findById(req.params.id, (err, foundStatus) => {
 			if (err) {
-				req.flash("error", "Status not found");
-				res.redirect("back");
+				util.flashMsg(req, res, false, "Status not found", "back");
 			} else {
 				if (foundStatus.author.id.equals(req.user._id)) {
 					next();
 				} else {
-					req.flash("error", "You don't have permission to do that");
-					res.redirect("back");
+					util.flashMsg(
+						req,
+						res,
+						false,
+						"You don't have permission to do that",
+						"back"
+					);
 				}
 			}
 		});
 	} else {
-		req.flash("error", "You must be logged in to do that");
-		res.redirect("/");
+		util.flashMsg(req, res, false, "You must be logged in to do that", "/");
 	}
 };
+
+
 
 // Checks if a comment is owned by the user trying to update that comment
 middlewareObj.checkCommentOwnership = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		Comment.findById(req.params.commentId, (err, foundComment) => {
 			if (err) {
-				req.flash("error", "Comment not found");
-				res.redirect("back");
+				util.flashMsg(req, res, false, "Comment not found", "back");
 			} else {
 				if (foundComment.author.id.equals(req.user._id)) {
 					next();
 				} else {
-					req.flash("error", "You don't have permission to do that");
-					res.redirect("back");
+					util.flashMsg(
+						req,
+						res,
+						false,
+						"You don't have permission to do that",
+						"back"
+					);
 				}
 			}
 		});
 	} else {
-		req.flash("error", "You must be logged in to do that");
-		res.redirect("/");
+		util.flashMsg(req, res, false, "You must be logged in to do that", "/");
 	}
 };
+
+
 
 // Checks if a user is logged in
 middlewareObj.isLoggedIn = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		return next();
-	}
-	req.flash("error", "You must be logged in to do that");
-	res.redirect("/");
+  }
+  
+	util.flashMsg(req, res, false, "You must be logged in to do that", "/");
 };
 
 module.exports = middlewareObj;
